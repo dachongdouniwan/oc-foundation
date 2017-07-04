dd1ba8b3dc0c5c41a313c701f2722be3d9c08359
fallen.ink <fengzilijie@qq.com>
2017-07-04 23:41:28 +0800

    add compute module

---
+++ compute/_math.m
@@ -0,0 +1,13 @@
+//
+//  _math.m
+//  Ben
+//
+//  Created by fallen.ink on 04/07/2017.
+//  Copyright © 2017 fallen.ink. All rights reserved.
+//
+
+#import "_math.h"
+
+@implementation _math
+
+@end
